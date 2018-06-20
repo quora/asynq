3927da1f2466945d78507c87d0a9cc155da1e1a1
Kenny Do <kedo@patreon.com>
2018-06-19 22:50:39 -0700

    remove py33 from classifiers

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ if __name__ == '__main__':
 
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
