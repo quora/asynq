f4908b2bc91ec8d0d9b04e7732ed5f8c4d167987
Kenny Do <chinesedewey@gmail.com>
2018-08-09 12:47:30 -0700

    Build wheels in Travis (#67)

--- setup.py
+++ setup.py
@@ -65,10 +65,10 @@ if __name__ == '__main__':
 
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
         ],
         keywords='quora asynq common utility',
         packages=['asynq', 'asynq.tests'],
