4cdc7a669cd5789944c149ca902b56262af5ee14
dkang-quora <135926539+dkang-quora@users.noreply.github.com>
2024-07-17 07:06:55 +0900

    Further improve typing (#145)
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- asynq/tests/test_typing.py
+++ asynq/tests/test_typing.py
@@ -1,7 +1,10 @@
-from typing import Any, Generator, TYPE_CHECKING
+from typing import Any, Generator, TYPE_CHECKING, TypeVar
 from typing_extensions import assert_type
 from asynq.decorators import async_call, lazy, asynq
 from asynq.futures import FutureBase
+from asynq.tools import amax
+
+_T = TypeVar("_T")
 
 
 def test_lazy() -> None:
@@ -23,13 +26,25 @@ def test_dot_asyncio() -> None:
         yield None
         return str(x)
 
-    async def caller() -> None:
-        # This doesn't work, apparently due to a mypy bug
-        assert_type(await generator.asyncio(1), str)  # type: ignore[assert-type]
-        assert_type(await non_generator.asyncio(1), str)  # type: ignore[assert-type]
+    @asynq()
+    def generic(x: _T) -> _T:
+        return x
+
+    class Obj:
+        @asynq()
+        def method(self, x: str) -> int:
+            return int(x)
+
+    async def caller(x: str, obj: Obj) -> None:
+        assert_type(await generator.asyncio(1), Any)  # TODO: str
+        assert_type(await non_generator.asyncio(1), Any)  # TODO: str
+        assert_type(await generic.asyncio(x), Any)  # TODO: str
+        assert_type(await obj.method.asyncio(x), Any)  # TODO: int
 
         await non_generator.asyncio()  # type: ignore[call-arg]
         await generator.asyncio()  # type: ignore[call-arg]
+        await generic.asyncio(1, 2)  # type: ignore[call-arg]
+        await obj.method.asyncio(1)  # type: ignore[arg-type]
 
 
 def test_async_call() -> None:
@@ -39,3 +54,9 @@ def test_async_call() -> None:
     async_call(f, 1)
     if TYPE_CHECKING:
         async_call(f, 1, task_cls=FutureBase)  # TODO: this should be an error
+
+
+def test_amax(x: int = 1, y: int = 2) -> None:
+    if TYPE_CHECKING:
+        assert_type(amax(1, 2, key=len), Any)  # TODO: int
+        assert_type(amax([1, 2], key=len), Any)  # TODO: int
